a96a5eb685ba0959da5d44f3e62e0f6f8ec9b258
ANKUNDA MWEBEMBEZI <105851470+Ankz77@users.noreply.github.com>
2022-07-23 10:59:57 +0300

    Update restaurant sql.py

--- dbms assignment/restaurant sql.py
+++ dbms assignment/restaurant sql.py
@@ -13,7 +13,7 @@ db.execute("CREATE TABLE waiter(id INTEGER PRIMARY KEY, waiters TEXT, customer_i
 
 db.execute("CREATE TABLE food(id INTEGER PRIMARY KEY,cook TEXT, meal TEXT, drink TEXT, amount INTEGER, waiters_id INTEGER, FOREIGN KEY(waiters_id) REFERENCES waiter(id));")
 
-db.execute("CREATE TABLE link(customer_id INTEGER, cook_id INTEGER, waiters_id INTEGER, FOREIGN KEY(customer_id) REFERENCES customer(id), FOREIGN KEY(waiters_id) REFERENCES waiter(id), FOREIGN KEY(cook_id) REFERENCES food(id));")
+#db.execute("CREATE TABLE link(customer_id INTEGER, cook_id INTEGER, waiters_id INTEGER, FOREIGN KEY(customer_id) REFERENCES customer(id), FOREIGN KEY(waiters_id) REFERENCES waiter(id), FOREIGN KEY(cook_id) REFERENCES food(id));")
 
 with open("restaurant.csv", "r") as file:
     reader=csv.DictReader(file)
@@ -22,7 +22,7 @@ with open("restaurant.csv", "r") as file:
         num1=row["id"]
         cust=row["customer"]
         num2=row["table_no"]
-        tab=row["waiters_id"]
+        tab=row["id"]
         fine=row["waiters"]
         go=row["order_to_go"]
         num3=row["cook_id"]
@@ -33,7 +33,7 @@ with open("restaurant.csv", "r") as file:
 
         db.execute("INSERT INTO customer(id,name,table_no,order_to_go) VALUES(?,?,?,?)",num1,cust,num2,go)
 
-        db.execute("INSERT INTO waiter(waiters) VALUES(?)",fine)
+        db.execute("INSERT INTO waiter(id,waiters) VALUES(?,?)",tab,fine)
 
         db.execute("INSERT INTO food(cook,meal,drink,amount) VALUES(?,?,?,?)",chef,food,soft,cost)
 
